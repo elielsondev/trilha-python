fa7f8a16fdf47208d23e52d73c0921d446da8e94
Elielson Nascimento <nascimento.elielson@gmail.com>
2023-02-20 18:09:27 -0300

    feat - basic operations in Python

---
+++ basic_operations.py
@@ -0,0 +1,40 @@
+number1 = 10
+number2 = 5
+
+sum = number1 + number2
+print(sum)  # 15
+
+subtraction = number1 - number2
+print(subtraction)  # 5
+
+# O * é para multiplicação
+multplication = number1 * number2
+print(multplication)  # 50
+
+# O ** é para potenciação
+potentiation = number1 ** number2
+print(potentiation)  # 100000
+
+# Com o / a divisão é float
+divisionFloat = number1 / number2
+print(divisionFloat)  # 2.0
+
+# Com // a divisão é inteiro
+divisionInteger = number1 // number2
+print(divisionInteger)  # 2
+
+
+# Incremento ou Decremento em Python não aceita essa sintaxe ++ e --
+counter = 10
+
+# Os exemplos de sintaxe validas são
+
+# Incremento
+# counter = counter + 1
+counter += 1
+print(counter)  # 11
+
+# Decremento
+# counter = counter - 1
+counter -= 1
+print(counter)  # 10
